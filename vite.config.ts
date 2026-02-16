6dc5fa27345c13cd0c997c41ea70a5d8482f540d
Esther Ndung'u <esther.w.ndungu.n@gmail.com>
2026-02-16 17:40:42 +0300

    Remove server allowedHosts configuration and set base
    Edited Vite based on Github Actions as page was not rendering on pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true,
-  },
+  base: "serverless-architecture-k5m5vk",
 });
+
  
