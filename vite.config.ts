421fd5b7fd3413afc483ae7a9f415299a637f1e1
Esther Ndung'u <esther.w.ndungu.n@gmail.com>
2026-02-16 17:41:22 +0300

    Update base path in Vite configuration
    Added slashes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "serverless-architecture-k5m5vk",
+  base: "/serverless-architecture-k5m5vk/",
 });
 
  
